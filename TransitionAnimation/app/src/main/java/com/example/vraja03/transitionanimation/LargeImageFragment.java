94610dff9970575918c99186cc6d6e75ac2c2611
Vimalathithan Rajasekaran <vimalathithanit@gmail.com>
2016-09-01 17:17:39 -0700

    Initial Commit

---
+++ TransitionAnimation/app/src/main/java/com/example/vraja03/transitionanimation/LargeImageFragment.java
@@ -0,0 +1,22 @@
+package com.example.vraja03.transitionanimation;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by VRAJA03 on 9/1/2016.
+ */
+
+public class LargeImageFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+}
